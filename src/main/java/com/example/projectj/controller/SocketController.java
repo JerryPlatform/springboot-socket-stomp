5b6b6e9d11116cf36f40400148d374540b6a73b3
Seong Min Ju <smju@wkit.co.kr>
2022-02-16 08:51:01 +0900

    [fix]: CI/CD 테스트

--- src/main/java/com/example/projectj/controller/SocketController.java
+++ src/main/java/com/example/projectj/controller/SocketController.java
@@ -40,7 +40,6 @@ public class SocketController {
     @PostMapping("/chat/room/in")
     public void roomIn(@RequestBody SocketVo vo) throws JsonProcessingException {
         Room room = roomService.findRoom(vo.getRoomId());
-        log.info("★");
         String userName = "[알림]";
         String content = vo.getUserName() + "님이 채팅방에 입장하였습니다.";
 
