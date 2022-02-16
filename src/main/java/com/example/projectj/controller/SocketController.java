bd1d37bd1c6caea2c5bdd3d387d9e3cb971eec82
Seong Min Ju <smju@wkit.co.kr>
2022-02-16 17:29:55 +0900

    [fix]: 채팅방 리스트 스케줄 주기 갱신

--- src/main/java/com/example/projectj/controller/SocketController.java
+++ src/main/java/com/example/projectj/controller/SocketController.java
@@ -8,23 +8,25 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpSubscription;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Log
 @RestController
 @EnableScheduling
 @RequiredArgsConstructor
 public class SocketController {
+
+    private SimpUserRegistry simpUserRegistry;
 
     private final SimpMessagingTemplate template;
 
@@ -33,6 +35,13 @@ public class SocketController {
     @PostMapping("/room")
     public void createRoom(@RequestBody RoomDto dto) {
         roomService.createRoom(dto.getRoomName());
+    }
+
+    void find() {
+        Set<SimpSubscription> user = simpUserRegistry.findSubscriptions(subscription -> subscription.equals("/sub/chat/room/1"));
+        for (SimpSubscription subscription : user) {
+
+        }
     }
 
     @DeleteMapping("/{id}/room")
@@ -74,7 +83,7 @@ public class SocketController {
         template.convertAndSend("/sub/chat/room/" + room.getId(), resultValue);
     }
 
-    @Scheduled(fixedDelay = 6000)
+    @Scheduled(fixedDelay = 1000)
     @SendTo("/sub/room/list")
     public void roomList() throws JsonProcessingException {
         List<Room> roomList = roomService.getRoomList();
