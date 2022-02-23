6919b24e6291461f917217ce3a0f93eeeca8e2da
Seong Min Ju <smju@wkit.co.kr>
2022-02-23 16:23:21 +0900

    [feat]: graphQL List<Map> test

--- src/main/java/com/example/projectj/graphQL/Room/RoomQuery.java
+++ src/main/java/com/example/projectj/graphQL/Room/RoomQuery.java
@@ -11,7 +11,7 @@ import lombok.extern.java.Log;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -33,5 +33,18 @@ public class RoomQuery implements GraphQLQueryResolver {
         List<RoomVo> result = roomList.stream().map(mapRoom).collect(Collectors.toList());
 
         return Response.builder().response(Result.builder().build()).contents(result).build();
+    }
+
+    public List<Map<String, String>> getMapData() {
+        List<Map<String, String>> result = new ArrayList<>();
+        Random random = new Random();
+        for (int i=1; i<6; i++) {
+            Map<String, String> map = new HashMap<>();
+            map.put("id", String.valueOf(i));
+            map.put("value", String.valueOf(random.nextInt()));
+            result.add(map);
+        }
+
+        return result;
     }
 }
